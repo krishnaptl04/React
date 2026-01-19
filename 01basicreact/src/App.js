9fbb9e7446fb88553512c633f8e7b9ef05dce22a
krishna <krishnaptl0414@gmail.com>
2026-01-19 13:26:43 +0530

    second commit

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+    <h1>REACT</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>REACT WITH VITE | KRISHNA PATEL</h1>;
+}
+
+export default App;
