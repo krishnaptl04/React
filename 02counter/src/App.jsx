922a9442456abb7c7b664e65cf62bfb320275dbb
krishna <krishnaptl0414@gmail.com>
2026-01-21 13:06:22 +0530

    add counter

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  let counter = 15;
+
+  const addValue = () => {
+    // console.log("value added", Math.random());
+    console.log("clicked", counter);
+    counter = counter + 1;
+  };
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <h2>Counter Value : {counter}</h2>
+
+      <button onClick={addValue}>Add Value</button>
+      <br />
+      <br />
+      <button>Remove Value</button>
+    </>
+  );
+}
+
+export default App;
