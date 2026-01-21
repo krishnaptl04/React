35bc0103ee079d12c7c2ebe2bae36c8fabc4fa32
krishna <krishnaptl0414@gmail.com>
2026-01-21 12:24:06 +0530

    03customreact completed

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,12 +1,14 @@
 import Tea from "./K";
 
 function App() {
+  const username = "Krishna";
   return (
     <>
       <Tea />
-      <h1>Tea Aur React</h1>
+      <h1>Tea Aur React {username}</h1>
       <p>Test Para</p>
     </>
   );
 }
+
 export default App;
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,6 +1,36 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(<App />);
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom App | Tea</h1>
+    </div>
+  );
+}
+
+// const ReactElement = {
+//   type: "a",
+//   props: {
+//     href: "https://google.com",
+//     target: "_blank",
+//   },
+//   children: "Click Me To Visit Google",
+// };
+
+const anotherElement = (
+  <a href="https://google.com" target="_blank">
+    Visit Google
+  </a>
+);
+const anotherUser = "KRISHNA";
+
+const reactElement = React.createElement(
+  "a",
+  { href: "https://google.com", target: "_blank" },
+  "click me to visit goggle",
+  anotherUser,
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(reactElement);
